0579087d8ef2d6b78cacb76b73ef58ce4d094b71
Skywt2003 <Skywt2003@gmail.com>
2022-03-29 19:20:29 +0800

    ✨ 在 Navbar 固定时显示阴影

--- header.php
+++ header.php
@@ -95,4 +95,14 @@
     </ul>
 </nav>
 
+<!-- Add shadow for navbar when fixed. -->
+<script>
+    const stickyElm = document.querySelector('.navbar')
+    const observer = new IntersectionObserver( 
+      ([e]) => e.target.classList.toggle('shadow', e.intersectionRatio < 1),
+      {threshold: [1]}
+    );
+    observer.observe(stickyElm);
+</script>
+
 <main class="container">
